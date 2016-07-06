11a187c804936be126cd33575eb28eef5df05cd2
ryichang <ryichang@gmail.com>
2016-07-06 14:48:33 -0700

    Changed production

--- env/production.js
+++ env/production.js
@@ -2,7 +2,7 @@ var port = process.env.PORT;
 
 module.exports = {
   port: port,
-  db: process.env.MONGOLAB_URI,
+  db: process.env.MONGODB_URI,
   TOKEN_SECRET: process.env.TOKEN_SECRET,
   FACEBOOK_SECRET: process.env.FACEBOOK_SECRET,
   GOOGLE_SECRET: process.env.GOOGLE_SECRET
