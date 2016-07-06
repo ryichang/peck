62b42f43a12bbb22e31a4000b6e96b9680174517
ryichang <ryichang@gmail.com>
2016-07-06 14:46:08 -0700

    Changed production.js

--- env/production.js
+++ env/production.js
@@ -2,9 +2,7 @@ var port = process.env.PORT;
 
 module.exports = {
   port: port,
-  db: process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/peckbox',
+  db: process.env.MONGOLAB_URI,
   TOKEN_SECRET: process.env.TOKEN_SECRET,
   FACEBOOK_SECRET: process.env.FACEBOOK_SECRET,
   GOOGLE_SECRET: process.env.GOOGLE_SECRET
