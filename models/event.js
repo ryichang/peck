f769024b6b705aafd00c62bbcce371a67d01ff44
ryichang <ryichang@gmail.com>
2016-07-17 13:38:31 -0700

    Remove middleware for Event Date

--- models/event.js
+++ models/event.js
@@ -15,16 +15,16 @@ var EventSchema = Schema({
 });
 
 
-// MIDDLEWARE
-EventSchema.pre('save', function(next){
-  // set a created_at and update updated_at
-  now = new Date();
-  this.updated_at = now;
-  if ( !this.created_at ) {
-    this.created_at = now;
-  }
-  next();
-});
+// // MIDDLEWARE
+// EventSchema.pre('save', function(next){
+//   // set a created_at and update updated_at
+//   now = new Date();
+//   this.updated_at = now;
+//   if ( !this.created_at ) {
+//     this.created_at = now;
+//   }
+//   next();
+// });
 
 
 
