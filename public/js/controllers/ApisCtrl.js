32fd299d2b5424301a23d594d8ebe493d7eed096
ryichang <ryichang@gmail.com>
2016-07-19 02:29:15 -0700

    Added scope for size and color

--- public/js/controllers/ApisCtrl.js
+++ public/js/controllers/ApisCtrl.js
@@ -114,8 +114,12 @@ var api = 'https://api.nytimes.com/svc/news/v3/content/all/all.jsonp?api-key=ccb
         var key = "598aeaa830f0e56213a7a3401ab14bf1/"
         $http.jsonp(url + key + lat + "," + lon + "?callback=JSON_CALLBACK").success(function(response){
           $scope.weather = response;
-
-          console.log('ICON IS', $scope.icon)
+          $scope.CurrentWeather = {
+                forecast: {
+                    iconSize: 100,
+                    color: "white",
+                   
+                }
           console.log('ApiCtrl', $scope.weather)
         });
       });
--- public/vendor/angular-skycons/angular-skycons.js
+++ public/vendor/angular-skycons/angular-skycons.js
@@ -25,7 +25,7 @@ angularSkycons.directive( "skycon", function () {
 
             // set default color if "color" attribute not present
             var config = {
-                color: scope.color || "white"
+                color: scope.color || "black"
             };
 
             var skycons = new Skycons( config );
