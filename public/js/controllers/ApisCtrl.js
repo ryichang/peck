510f1960a1c1ce8a8a0cbae36c08e81ecb17503e
ryichang <ryichang@gmail.com>
2016-07-28 03:22:35 -0700

    testing toastr for weather

--- public/js/controllers/ApisCtrl.js
+++ public/js/controllers/ApisCtrl.js
@@ -120,7 +120,7 @@ function success(position) {
                 color: "white",
             }
       };
-      toastr.info('Current weather is '+ weather.currently.summary + '' + weather.currently.temperature + "°"); 
+      toastr.warning('Current weather is '+ weather.currently.summary + '' + weather.currently.temperature + "°"); 
       console.log('ApiCtrl', $scope.weather)
     });
   });
