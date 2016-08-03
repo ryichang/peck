cd769ebd40819e1be4d60d83e292b27ded2e2580
ryichang <ryichang@gmail.com>
2016-08-03 12:44:26 -0700

    Changed fantasy url from JSON to JSONP

--- public/js/controllers/ApisCtrl.js
+++ public/js/controllers/ApisCtrl.js
@@ -32,7 +32,7 @@ var api = 'https://api.nytimes.com/svc/news/v3/content/all/all.jsonp?api-key=ccb
                 });
 
   $scope.sports={}
-  var url="https://fantasydata.com/rss/rotoworld/?format=json&callback=JSON_CALLBACK";
+  var url="https://fantasydata.com/rss/rotoworld/?format=jsonp&callback=JSON_CALLBACK";
   $http.jsonp(url).success(function(data){
     console.log('sport data is', data);
   })
