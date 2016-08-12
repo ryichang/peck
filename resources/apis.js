d3bdbd50f5f10ea5cbffb81c582582c392856373
ryichang <ryichang@gmail.com>
2016-08-12 15:41:24 -0700

    Added res.send

--- resources/apis.js
+++ resources/apis.js
@@ -1,19 +1,20 @@
-// var auth = require('./auth'),
-// 		config = require('../config.js'),
-// 		request = require('request');
+var auth = require('./auth'),
+		config = require('../config.js'),
+		request = require('request');
 
-// module.exports = function(app) {
-// 	app.post('/api/sportsnews', function(req,res){
-// 		console.log('API CALL')
-// 		var url ="https://fantasydata.com/rss/rotoworld/?format=jsonp&callback=JSONP_CALLBACK";
-// 		request(url, function(err, response){
-// 			if (error) {
-// 				console.log("error is", err);
-// 			} 
-// 				console.log('data is', response);
-// 				res.send(response);
-// 		});
-// 	});
-// } ;
+module.exports = function(app) {
+	app.post('/api/sportsnews', function(req,res){
+		console.log('API CALL')
+		var url ="https://fantasydata.com/rss/rotoworld/?format=jsonp&callback=JSONP_CALLBACK";
+		res.status(200).send("api works");
+		// request(url, function(err, response){
+		// 	if (error) {
+		// 		console.log("error is", err);
+		// 	} 
+		// 		console.log('data is', response);
+		// 		res.send(response);
+		// });
+	});
+} ;
 
 
--- resources/users.js
+++ resources/users.js
@@ -9,18 +9,6 @@ var User = require('../models/user.js')
 
 module.exports = function(app) {
   
-  app.post('/api/sportsnews', function(req,res){
-    console.log('API CALL')
-    var url ="https://fantasydata.com/rss/rotoworld/?format=jsonp&callback=JSONP_CALLBACK";
-    request(url, function(err, response){
-      if (error) {
-        console.log("error is", err);
-      } 
-        console.log('data is', response);
-        res.send(response);
-    });
-  });
-
   app.get('/api/me', auth.ensureAuthenticated, function(req, res) {
     User.findOne({ _id: req.userId })
         .populate('posts')
