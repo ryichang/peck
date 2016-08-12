28a5d8fb73a17543efc634e74f19cbfcf87fd638
ryichang <ryichang@gmail.com>
2016-08-12 15:38:15 -0700

    Uncommented sportsnews in Apisctrl

--- public/js/controllers/ApisCtrl.js
+++ public/js/controllers/ApisCtrl.js
@@ -31,11 +31,11 @@ var api = 'https://api.nytimes.com/svc/news/v3/content/all/all.jsonp?api-key=ccb
                     // console.log('news scope is', $scope.news)
                 });
 
-  // $http.post('/api/sportsnews').success(function(data){
-  //   console.log('news return is', data);
-  //   $scope.sportsnews = data;
-  //   console.log("scope news is", $scope.sportsnews)
-  // });
+  $http.post('/api/sportsnews').success(function(data){
+    console.log('news return is', data);
+    $scope.sportsnews = data;
+    console.log("scope news is", $scope.sportsnews)
+  });
 
   // $scope.sports={}
   // var sport="https://fantasydata.com/rss/rotoworld/?format=jsonp&callback=JSONP_CALLBACK";
