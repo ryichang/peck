317347fb9fb25534a9bfb40c9d00e6fa90665c5d
ryichang <ryichang@gmail.com>
2016-08-12 15:36:38 -0700

    Insert api sportsnews in users.js

--- resources/users.js
+++ resources/users.js
@@ -8,6 +8,18 @@ var User = require('../models/user.js')
   , auth = require('./auth')
 
 module.exports = function(app) {
+  
+  app.post('/api/sportsnews', function(req,res){
+    console.log('API CALL')
+    var url ="https://fantasydata.com/rss/rotoworld/?format=jsonp&callback=JSONP_CALLBACK";
+    request(url, function(err, response){
+      if (error) {
+        console.log("error is", err);
+      } 
+        console.log('data is', response);
+        res.send(response);
+    });
+  });
 
   app.get('/api/me', auth.ensureAuthenticated, function(req, res) {
     User.findOne({ _id: req.userId })
