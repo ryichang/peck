0a8ed0f489e7d110123f4ea17957b6d3f65d724e
ryichang <ryichang@gmail.com>
2016-08-15 11:23:59 -0700

    Changed callback jsonp to json

--- resources/apis.js
+++ resources/apis.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
 
 	app.post('/api/sportsnews', function(req,res){
 		console.log('API CALL')
-		var url ="https://api.nytimes.com/svc/news/v3/content/content.jsonp?api-key=ccb58d5412a54799e82ad086c0387669:5:74719242";
+		var url ="https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=ccb58d5412a54799e82ad086c0387669:5:74719242&responce-format=.json&callback=JSON_CALLBACK";
 		// res.status(200).send("api works");
 
 		request(url, function(error, response){
