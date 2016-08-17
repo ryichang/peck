8ce6c6beedb831c462ba4f7a222e7f68971dfad3
ryichang <ryichang@gmail.com>
2016-08-17 11:43:39 -0700

    Removed err in events.js save route

--- resources/events.js
+++ resources/events.js
@@ -67,10 +67,12 @@ module.exports = function(app) {
 
 	app.post('/api/events', auth.ensureAuthenticated, function (req,res) {
 		User.findById(req.userId).exec(function(err, user) {
+			// if (err)
+			// 	res.send(err);
 			var event = new Event(req.body);
 			event.save(function(err, event) {
-				if (err) 
-					res.send(err);
+				// if (err) 
+				// 	res.send(err);
 				user.events.unshift(event._id);
 				user.save();
 				res.send(event);				
