a2ec66816aeb83c7cdbabb009cb1cd7dee157f27
ryichang <ryichang@gmail.com>
2016-08-17 11:39:53 -0700

    Removed err in events.js

--- resources/events.js
+++ resources/events.js
@@ -67,12 +67,8 @@ module.exports = function(app) {
 
 	app.post('/api/events', auth.ensureAuthenticated, function (req,res) {
 		User.findById(req.userId).exec(function(err, user) {
-			if (err)
-				res.send(err);
 			var event = new Event(req.body);
 			event.save(function(err, event) {
-				if (err) 
-					res.send(err);
 				user.events.unshift(event._id);
 				user.save();
 				res.send(event);				
