f14f75ae40322b15699ea1238cf57224bc5739a0
ryichang <ryichang@gmail.com>
2016-08-26 13:33:31 -0700

    Removed toastr from weather

--- public/js/controllers/ApisCtrl.js
+++ public/js/controllers/ApisCtrl.js
@@ -9,25 +9,25 @@ angular.module('peckbox')
     });
 
   $scope.news={}
-  $scope.types = ['hot'];         
-  $scope.subredit="worldnews";      
+  $scope.types = ['hot'];
+  $scope.subredit="worldnews";
   $scope.type="top";
   var url="https://api.reddit.com/r/"+$scope.subredit+"/?jsonp=JSON_CALLBACK";
   $http.jsonp(url).success(function(data) {
     $scope.elements = [];
     var dataset = data.data.children;
     for (var i=0; i<dataset.length; i++ ){
-          $scope.elements.push(dataset[i].data); // response data 
+          $scope.elements.push(dataset[i].data); // response data
           // console.log('reddit', $scope.elements);
-        }    
-    // toastr.warning('Loading Top Worldnews from Reddit');   
+        }
+    // toastr.warning('Loading Top Worldnews from Reddit');
       });
 
-// var api = 'https://api.nytimes.com/svc/news/v3/content/all/all.jsonp?api-key=ccb58d5412a54799e82ad086c0387669:5:74719242&responce-format=.jsonp&callback=JSON_CALLBACK'; 
+// var api = 'https://api.nytimes.com/svc/news/v3/content/all/all.jsonp?api-key=ccb58d5412a54799e82ad086c0387669:5:74719242&responce-format=.jsonp&callback=JSON_CALLBACK';
 //                 $http.jsonp(api).success(function(data){
 //                     console.log('news data is', data)
 //                     $scope.news = data.results;
-//                     // toastr.warning('Loading latest published articles from New York Times');  
+//                     // toastr.warning('Loading latest published articles from New York Times');
 //                     // console.log('news scope is', $scope.news)
 //                 });
 
@@ -70,16 +70,16 @@ angular.module('peckbox')
 
 //WEATHER
 navigator.geolocation.getCurrentPosition(success, error);
-function success(position) {  
+function success(position) {
   var coords = position.coords;
   $scope.$apply(function(){
         $scope.position = position;
         // console.log('position is', position);
         var lat = position.coords.latitude;
-        var lon = position.coords.longitude;    
+        var lon = position.coords.longitude;
         var query = lat + "," + lon;
         var url = "https://api.wunderground.com/api/df7130ecdd31c499/geolookup/q/";
-     
+
         $http.jsonp(url + query + ".json" +"?callback=JSON_CALLBACK").success(function(response){
           $scope.location = response;
           // console.log('ApiCtrl', $scope.location)
@@ -89,7 +89,7 @@ function success(position) {
     $scope.position = position;
     // console.log('position is', position);
     var lat = position.coords.latitude;
-    var lon = position.coords.longitude;    
+    var lon = position.coords.longitude;
     var query = "lat=" + lat + "&lon=" + lon;
     var url ="https://api.forecast.io/forecast/";
     var key = "598aeaa830f0e56213a7a3401ab14bf1/";
@@ -101,14 +101,14 @@ function success(position) {
                 color: "white",
             }
       };
-      toastr.info('Current weather is '+ response.currently.summary + ' ' + 'at '+ response.currently.temperature + "°F." + ' ' + response.daily.summary); 
+      // toastr.info('Current weather is '+ response.currently.summary + ' ' + 'at '+ response.currently.temperature + "°F." + ' ' + response.daily.summary); 
       // console.log('ApiCtrl', $scope.weather)
     });
   });
   // console.log('Your current position is ' + coords.latitude + ' X ' + coords.longitude);
 }
 
-function error(err) {  
+function error(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
 }
 
@@ -121,7 +121,7 @@ function itsaclock(){
     var d = new Date(), h = d.getHours(), m=d.getMinutes();
     var hour = ' '+(m>32 ? hLabels[(h%12) + 1] : hLabels[h%12]) + (m%58<3? ' o\'clock':'');
     var min  = m===0?'':' '+(m<33 ? mLabels[Math.round(m/5)] : mLabels[6-Math.round((m-30)/5)]);
-    var approx = m%5===0?'':m%5>2?' nearly':' just after' ; 
+    var approx = m%5===0?'':m%5>2?' nearly':' just after' ;
     var topast = m%58<3 ? '' : m%60>32? ' to':' past';
     clock.innerHTML= 'It\'s' + approx + min + topast + hour;
     // clock.innerHTML='&#10077'+'It\'s' + approx + min + topast + hour +'&#10078';
@@ -131,6 +131,3 @@ setTimeout(itsaclock, 2000);
 
 
 }]);
-
-
-
